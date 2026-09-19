cff0ad8149f5dbab5201f695ce2ce2d76a5bcc5e
qhchina666 <qhcchina666@outlook.com>
2026-09-19 18:09:12 +0800

    gengxin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EasyCover - AcoFork
+# EasyCover - QHC
 
 <img width="1893" height="1390" alt="a7db63bb841d2b78d604e2662c50365c" src="https://github.com/user-attachments/assets/e8379b3a-2806-4069-ab47-9a7487c1dd82" />
 
@@ -86,4 +86,4 @@ npm run dev
 
 ---
 
-Made with ❤️ by AcoFork
+Made with ❤️ by QHC
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "EasyCover - AcoFork",
-  description: "EasyCover - AcoFork",
+  title: "EasyCover - QHC",
+  description: "EasyCover - QHC",
   icons: {
-    icon: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0",
+    icon: "https://imghub.imqhc.top/file/ico/Tp7jbwUY.webp",
   },
 };
 
